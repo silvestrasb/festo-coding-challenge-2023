bd9c017123467cd17460f975aab87c321fd8c648
Bumblauskas <lt0sbmk@festo.net>
2023-10-18 14:45:15 +0300

    Chapter 1 refactoring, added solutions cypher plate files.

--- src/main/java/com/festo/codingchallenge2023/chapter/_1/puzzle/_1/Main.java
+++ src/main/java/com/festo/codingchallenge2023/chapter/_1/puzzle/_1/Main.java
@@ -1,24 +1,25 @@
 package com.festo.codingchallenge2023.chapter._1.puzzle._1;
 
+import com.festo.codingchallenge2023.chapter.util.general.file.PathResolver;
 import com.festo.codingchallenge2023.chapter.util.keymaker.exception.InvalidInstructionException;
+import com.festo.codingchallenge2023.chapter.util.keymaker.file.KeyMakerFileReader;
 import com.festo.codingchallenge2023.chapter.util.keymaker.model.Hammer;
 import com.festo.codingchallenge2023.chapter.util.keymaker.model.Recipe;
 import com.festo.codingchallenge2023.chapter.util.keymaker.service.HammerService;
 import com.festo.codingchallenge2023.chapter.util.keymaker.service.KeyService;
-import com.festo.codingchallenge2023.chapter.util.keymaker.file.KeyMakerFileReader;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 public class Main {
 
-    public static String KEYMAKER_RECIPE_ABS_PATH = "C:\\Appl\\repository\\festo-coding-challenge-2023\\src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_1\\resource\\11_keymaker_recipe.txt";
-    public static String KEYMAKER_HAMMER_CONFIG_ABS_PATH = "C:\\Appl\\repository\\festo-coding-challenge-2023\\src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_1\\resource\\hammer_collection.txt";
+    public static String KEYMAKER_RECIPE_REL_PATH = "src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_1\\resource\\11_keymaker_recipe.txt";
+    public static String KEYMAKER_HAMMER_CONFIG_REL_PATH = "src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_1\\resource\\hammer_collection.txt";
 
-    public static void main(String[] args) throws IOException {
-        List<Recipe> recipeList = KeyMakerFileReader.initializeRecipeList(KEYMAKER_RECIPE_ABS_PATH);
-        List<Hammer> hammerList = KeyMakerFileReader.initializeHammerList(KEYMAKER_HAMMER_CONFIG_ABS_PATH);
+    public static void main(String[] args) {
+        List<Recipe> recipeList = KeyMakerFileReader.initializeRecipeList(PathResolver.getAbsPath(KEYMAKER_RECIPE_REL_PATH));
+        List<Hammer> hammerList = KeyMakerFileReader.initializeHammerList(PathResolver.getAbsPath(KEYMAKER_HAMMER_CONFIG_REL_PATH));
+
         HammerService hammerService = new HammerService(hammerList);
         KeyService keyService = new KeyService(hammerService);
 
--- src/main/java/com/festo/codingchallenge2023/chapter/_1/puzzle/_3/Main.java
+++ src/main/java/com/festo/codingchallenge2023/chapter/_1/puzzle/_3/Main.java
@@ -1,7 +1,8 @@
 package com.festo.codingchallenge2023.chapter._1.puzzle._3;
 
-import com.festo.codingchallenge2023.chapter.util.trap.model.Trap;
+import com.festo.codingchallenge2023.chapter.util.general.file.PathResolver;
 import com.festo.codingchallenge2023.chapter.util.trap.file.TrapFileReader;
+import com.festo.codingchallenge2023.chapter.util.trap.model.Trap;
 import com.festo.codingchallenge2023.chapter.util.trap.validation.WeightedTrapSafetyChecker;
 
 import java.util.ArrayList;
@@ -9,10 +10,10 @@ import java.util.List;
 
 public class Main {
 
-    public static String TRAP_WEIGHT_CONFIG_ABS_PATH = "C:\\Appl\\repository\\festo-coding-challenge-2023\\src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_3\\resource\\13_trap_balance.txt";
+    private static final String TRAP_WEIGHT_CONFIG_REL_PATH = "src\\main\\java\\com\\festo\\codingchallenge2023\\chapter\\_1\\puzzle\\_3\\resource\\13_trap_balance.txt";
 
     public static void main(String[] args) {
-        List<Trap> trapList = TrapFileReader.initializeTraps(TRAP_WEIGHT_CONFIG_ABS_PATH);
+        List<Trap> trapList = TrapFileReader.initializeTraps(PathResolver.getAbsPath(TRAP_WEIGHT_CONFIG_REL_PATH));
         List<Trap> safeTraps = new ArrayList<>();
 
         for (Trap trap : trapList) {
